54336f1c74771e54ffaf2e129dbcaf04491f8de9
trakhabrix <tracyordono3@gmail.com>
2026-04-29 01:35:17 +0800

    Add expense menu function for user interaction

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,1 +1,9 @@
-
+def expense_menu():
+    print ("Expense Tracke")
+    print ("1 - Create/Input User ")
+    print ("2 - Update User")
+    print ("3 - Saved User Info")
+    print ("4 - Delete User Info")
+    print ("5 - Exit")
+    choice_menu = int(input("Enter your choice(1-5): "))
+expense_menu()
