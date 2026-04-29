f261bc0e2214463e7c6f8ce9c7ac8590577dd539
trakhabrix <tracyordono3@gmail.com>
2026-04-29 17:13:07 +0800

    Updated expense menu features (might change it later also

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,9 +1,18 @@
-def expense_menu():
-    print ("Expense Tracke")
-    print ("1 - Create/Input User ")
-    print ("2 - Update User")
-    print ("3 - Saved User Info")
-    print ("4 - Delete User Info")
-    print ("5 - Exit")
-    choice_menu = int(input("Enter your choice(1-5): "))
-expense_menu()
+while True:
+    def expense_menu():
+        print ("""Expense Tracker
+        1 - Create/Input User
+        2 - Update User
+        3 - Saved User Info
+        4 - Delete User Info
+        5 - Exit""")
+        
+        choice_menu = int(input("Enter your choice (1-5): "))
+        if choice_menu < 1 or choice_menu > 5:
+            print ("Invalid Choice")
+            choice_menu = int(input("Enter your choice (1-5): "))
+        elif choice_menu == " ":
+            print ("Please Enter Your Choice")
+            choice_menu = int(input("Enter your choice (1-5): "))
+
+    expense_menu()
