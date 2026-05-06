73b11b238bd030cd4ee7e4dc4099babb544e9f8a
trakhabrix <tracyordono3@gmail.com>
2026-05-06 11:40:32 +0800

    Add functions and View Function for Expense
    Implement functions to add and view expenses, including data storage.

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,3 +1,29 @@
+#Storage
+number_expense = 0
+expense_list = []
+#Create Expense
+def add_expense():
+    global number_expense
+    print("\n=====Add Section=====")
+    item = input("Input the Expense: ")
+    amount = float(input("Input the Amount: "))
+    number_expense += 1
+    #Store
+    expense_data = {"ID": number_expense, 
+                    "ITEM": item,
+                    "AMOUNT": amount}
+    expense_list.append(expense_data)
+    print (f"Expense Added +{number_expense}")
+
+
+#View EXpense
+def view_expense(): 
+    if not expense_list:
+        print("\nNot Yet Record")
+    print ("=====Expense List=====")
+    for expense in expense_list:
+        print(f"{expense['ID']}  {expense['ITEM']}  ₱{expense['AMOUNT']}\n")
+
 def expense_menu ():
     while True:
         print ("""====Expense Tracker====
